47383a1a72b83b1bd03e5608505ef3dc9190cd4e
Tonyx <antoine@svahn.fr>
2023-11-27 09:18:15 +0100

    modif 27-11

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'user', component: UserProfileComponent, data: { roles: ['admin', 'user'] } }
 ];
